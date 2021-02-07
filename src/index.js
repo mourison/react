0d4b3f7d55644a68cbce424a7f29c17600d96413
mourison <georgetsamtsouris@gmail.com>
2021-02-07 19:08:34 +0200

    Building contenet with JSX

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ import ReactDOM from 'react-dom';
 
 // create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = {text:'Click me!'};
+  const labelText = 'Enter name:'
+
+  return (
+    <div>
+      <label className = "label" htmlFor = "name">
+        {labelText}
+      </label>
+      <input id = "name" type = "text" />
+      <button style={{backgroundColor:'blue', color:'white'}}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 }
 
 
@@ -13,4 +26,4 @@ const App = () => {
 ReactDOM.render(
   <App/>,
   document.querySelector('#root')
-);
+); 
